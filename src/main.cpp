daf133468f821341ae7820eae1db50156e99da80
mrtuxa <mrtuxa@fedora.(none)>
2023-07-18 11:25:34 +0200

    changed NULL to nullptr

--- src/main.cpp
+++ src/main.cpp
@@ -8,20 +8,20 @@
 #include "include/screen.hpp"
 
 int main() {
-    SDL_Window* window = NULL;
+    SDL_Window* window = nullptr;
 
-    SDL_Surface* surface = NULL;
+    SDL_Surface* surface = nullptr;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not initialize! SDL_ERROR: " << SDL_GetError() << std::endl;
     } else {
         window = SDL_CreateWindow("SDL First App", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screen::width, screen::height, SDL_WINDOW_SHOWN);
 
-        if (window == NULL) {
+        if (window == nullptr) {
             std::cout << "Window could not be created! SDL_ERROR: " << SDL_GetError() << std::endl;
         } else {
             surface = SDL_GetWindowSurface( window );
-            SDL_FillRect( surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+            SDL_FillRect( surface, nullptr, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
 
             SDL_UpdateWindowSurface( window );
 
